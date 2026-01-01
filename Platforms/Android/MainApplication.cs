eda7c5a085dd0fb3abcf22a2af95b9ac35d0511d
Admin <Admin@HOMEPC>
2026-01-01 18:18:37 +0300

    Add project files.

---
+++ Platforms/Android/MainApplication.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Runtime;
+
+namespace YouSpent
+{
+    [Application]
+    public class MainApplication : MauiApplication
+    {
+        public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+            : base(handle, ownership)
+        {
+        }
+
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
